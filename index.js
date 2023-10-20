076e11e70facff79b87036a26d039e0bc7a70d74
Mohamed-Yousry-Ali <mohamedyousry631@gmail.com>
2023-10-20 18:02:45 +0300

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from 'express'
 import { connection } from './db/connection.js'
 import userRouts from './modules/users/user.routs.js'
 import taskRouts from './modules/tasks/task.routs.js'
+import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -11,6 +12,7 @@ app.use(express.json())
 connection();
  app.use(userRouts);
  app.use(taskRouts)
+ app.use(cors())
 // app.use(messageRoutes)
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
